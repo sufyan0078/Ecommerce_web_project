d0fc7927acf1d493a6e719e9823bb29e9f26aafa
Sufyan <sufyanansari0012@gmail.com>
2024-01-13 03:43:09 +0500

    Complete Registration process

--- shoppinglyx/app/views.py
+++ shoppinglyx/app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Customer,Product,Cart,OrderPlaced
 from django.views import View
-
+from .forms import UserRegistrationForm
+from django.contrib import messages
 # def home(request):
 #  return render(request, 'app/home.html')
 class ProductView(View):
@@ -37,14 +38,31 @@ def orders(request):
 def change_password(request):
  return render(request, 'app/changepassword.html')
 
-def mobile(request):
- return render(request, 'app/mobile.html')
+def mobile(request, data=None):
+ if data == None:
+  mobiles = Product.objects.filter(category="M")
+ elif data == "Oppo" or data == "Samsung":
+  mobiles = Product.objects.filter(category="M").filter(brand=data)
+ elif data == "Below":
+  mobiles = Product.objects.filter(category="M").filter(discounted_price__lt=50000)
+ elif data == "Above":
+  mobiles = Product.objects.filter(category="M").filter(discounted_price__gt=50000)
+ return render(request, 'app/mobile.html',{"mobiles":mobiles})
 
 def login(request):
  return render(request, 'app/login.html')
 
-def customerregistration(request):
- return render(request, 'app/customerregistration.html')
-
+# def customerregistration(request):
+#  return render(request, 'app/customerregistration.html')
+class CustomerRegistrationView(View):
+  def get(self, request):
+    form = UserRegistrationForm()
+    return render(request, 'app/customerregistration.html',{"form":form})
+  def post(self, request):
+    form = UserRegistrationForm(request.POST)
+    if form.is_valid():
+     messages.success(request, "Congratulation Registered Successfully")
+     form.save()
+    return render(request, 'app/customerregistration.html',{"form":form})
 def checkout(request):
  return render(request, 'app/checkout.html')
